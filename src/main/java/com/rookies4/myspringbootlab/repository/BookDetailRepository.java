88df98d6077203a9018876f2c27d89707637ca7c
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-08-21 22:24:25 +0900

    bookDetail&bookDetailRepository

---
+++ src/main/java/com/rookies4/myspringbootlab/repository/BookDetailRepository.java
@@ -0,0 +1,19 @@
+package com.rookies4.myspringbootlab.repository;
+
+import com.rookies4.myspringbootlab.entity.BookDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookDetailRepository extends JpaRepository<BookDetail, Long> {
+    Optional<BookDetail> findByBookId(Long bookId);
+    @Query("SELECT b FROM BookDetail b JOIN FETCH b.book WHERE b.id= :id")
+    Optional<BookDetail> findByIdWithBook(@Param("id") Long bookDetailId);
+
+    List<BookDetail> findByPublisher(String publisher);
+}
